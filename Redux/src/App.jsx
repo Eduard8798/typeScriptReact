a79c0ea6bd863b3f5ee2e649f17b847bfba8ee60
Eduard8798 <eduard9999997@gmail.com>
2025-08-25 22:49:26 +0300

    customers

--- Redux/src/App.jsx
+++ Redux/src/App.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import {useDispatch, useSelector} from "react-redux";
+import {addCustomerAction, removeCustomerAction} from "./store/customerReducer.js";
 
 const App = () => {
 
     const dispatch = useDispatch();
     const cash = useSelector(state => state.cash.cash)
+    const customers = useSelector(state => state.customers.customers)
+
     console.log(cash)
 
     const addCash = (cash) => {
@@ -13,12 +16,43 @@ const App = () => {
     const getCash = (cash) => {
         dispatch({type:"GET_CASH",payload: cash})
     }
+
+    const addCustomer = (name) =>{
+        const customer = {
+            name,
+            id: Date.now(),
+        }
+        dispatch(addCustomerAction(customer))
+    }
+
+    const removeCustomer = (customer) => {
+
+        dispatch(removeCustomerAction(customer.id))
+    }
+
     return (
         <div className={'app'}>
             <div style={{fontSize: '3rem'}}>{cash}</div>
             <div style={{display: 'flex'}}>
                 <button onClick={() => addCash(Number(prompt()))}>add cash</button>
                 <button onClick={() => getCash(Number(prompt()))}>get cash</button>
+                <button onClick={() => addCustomer(prompt())}>Add client</button>
+                <button onClick={() => getCash(Number(prompt()))}>deleted client</button>
+            </div>
+            <div style={{border: '1px solid black',padding: "10px",margin: '3px'}}>
+                {customers.length > 0 ?
+                <div>
+                    {customers.map(customer=>
+                    <div style={{border:'1px solid silver',padding:'4px',marginTop:'2px'}}
+                    onClick={()=> removeCustomer(customer)}
+                    >{customer.name}</div>
+                    )}
+                </div>
+                    :
+                    <div style={{fontSize: '3rem', marginTop:20}}>
+Client not found
+                    </div>
+                }
             </div>
         </div>
     );
--- Redux/src/store/index.js
+++ Redux/src/store/index.js
@@ -4,7 +4,7 @@ import {customReducer} from "./customerReducer.js";
 
 const rootReducer = combineReducers({
     cash:cashReducer,
-    custom: customReducer
+    customers: customReducer
 })
 
 export const store = createStore(rootReducer,window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
